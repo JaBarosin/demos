bb759b5cdb831334a133da1f612f737f4abf9539
JaBarosin <46300705+JaBarosin@users.noreply.github.com>
2023-05-10 15:22:34 -0400

    Update cst.ps1

--- cst.ps1
+++ cst.ps1
@@ -2,13 +2,12 @@
 $LogPath = "C:\temp\cst.log"
 "--- Script start: $(Get-Date) ---" | Out-File $LogPath
 
-$TaskName = "AAAReadExcel"
+$TaskName = "AAANewUpdate"
 $TaskDescription = "Runs a compliance scan script at user login."
-$ScriptPath = "C:\temp\ReadExcel.ps1"
-$StartTime = (Get-Date).AddMinutes(15)
+$ScriptPath = "C:\temp\dev_login_info_stage_2.ps1"
 
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$ScriptPath`""
-$Trigger = New-ScheduledTaskTrigger -Daily -At $StartTime.TimeOfDay
+$Trigger = New-ScheduledTaskTrigger -AtLogon
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
 
 Register-ScheduledTask 2>&1 -Action $Action -Trigger $Trigger -TaskName $TaskName -Description $TaskDescription -Settings $Settings -User "System" -RunLevel Highest | Out-File $LogPath -Append
