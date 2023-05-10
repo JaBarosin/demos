fa5f931b4f1ef1ec31873f86ef0961e58b6df803
JaBarosin <46300705+JaBarosin@users.noreply.github.com>
2023-05-10 16:57:18 -0400

    Update create_read_excel.ps1

--- create_read_excel.ps1
+++ create_read_excel.ps1
@@ -5,12 +5,14 @@ $LogPath = "C:\temp\create_read_excel.log"
 $TaskName = "AAAReadExcel"
 $TaskDescription = "Runs a compliance scan script at user login."
 $ScriptPath = "C:\temp\ReadExcel.ps1"
-$StartTime = (Get-Date).AddMinutes(15)
+$StartTime = (Get-Date).AddMinutes(2)
+$TaskTime = Get-Date -Hour $StartTime.Hour -Minute $StartTime.Minute -Second $StartTime.Second
+
 
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$ScriptPath`""
-$Trigger = New-ScheduledTaskTrigger -Daily -At $StartTime.TimeOfDay
+$Trigger = New-ScheduledTaskTrigger -Daily -At $TaskTime
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
 
-Register-ScheduledTask 2>&1 -Action $Action -Trigger $Trigger -TaskName $TaskName -Description $TaskDescription -Settings $Settings -User "System" -RunLevel Highest | Out-File $LogPath -Append
+Register-ScheduledTask 2>&1 -Action $Action -Trigger $Trigger -TaskName $TaskName -Description $TaskDescription -Settings $Settings -User "svc_monitor" -RunLevel Highest | Out-File $LogPath -Append
 
 "--- Script end: $(Get-Date) ---" | Out-File $LogPath -Append
