76561b8b158a29e04432cf650cf052738e1b55c2
JaBarosin <46300705+JaBarosin@users.noreply.github.com>
2023-05-10 17:04:08 -0400

    Create ReadExcel.ps1

---
+++ ReadExcel.ps1
@@ -0,0 +1,41 @@
+# Set log path
+$LogPath = "C:\temp\demosetup\read_excel.log"
+
+# Create an Excel.Application COM object
+$Excel = New-Object -ComObject Excel.Application
+
+$Excel.Visible = $false
+
+# Open the Excel file
+$FilePath = "C:\temp\demosheet5.xlsm"
+$Workbook = $Excel.Workbooks.Open($FilePath)
+
+# Log file open event
+"Opened file $FilePath at $(Get-Date)" | Out-File $LogPath -Append
+
+# Read the content from the first worksheet
+$Worksheet = $Workbook.Worksheets.Item(1)
+$Range = $Worksheet.UsedRange
+
+# Loop through the rows and columns and display the content in the PowerShell console
+for ($row = 1; $row -le $Range.Rows.Count; $row++) {
+    for ($col = 1; $col -le $Range.Columns.Count; $col++) {
+        $CellValue = $Worksheet.Cells.Item($row, $col).Value2
+        Write-Host -NoNewline "$CellValue       "
+        
+        # Log cell value
+        "Cell[$row, $col] value: $CellValue" | Out-File $LogPath -Append
+    }
+    Write-Host ""
+}
+
+# Close and release the Excel objects
+$Workbook.Close($false)
+$Excel.Quit()
+
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Worksheet)
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook)
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
+
+# Log completion
+"Script completed at $(Get-Date)" | Out-File $LogPath -Append
